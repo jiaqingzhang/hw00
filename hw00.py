4754d707b7a5b41735831b5e11acc94f2220838a
jiaqingzhang <70565908+jiaqingzhang@users.noreply.github.com>
2020-09-01 15:27:22 +0800

    Add files via upload

---
+++ hw00.py
@@ -0,0 +1,32 @@
+def hw00():
+
+	''' Assignment 00 for Foundations of Machine Learning, Fall 2020
+		Provide information on your background in programming, python, git, and machine learning. 
+		Provide insight into what you are hoping to learn in this class this semester. 
+	 	Submit your assignment as described in class by pushing to github. 
+		Do not include the brackets in your response.  
+	''' 
+
+	print('My name is:')
+	print('[jiaqing zhang]')
+	print('My github username is:')
+	print('[jiaqingzhang]')
+	print('I am a:')
+	print('[masters] student')
+	print('I am majoring in:')
+	print('[electrical and computer engineering]')
+	print(' My level of programming experience in general is:')
+	print('[some]')
+	print('My python programming experience is:')
+	print('[some]')
+	print('My git experience is:')
+	print('[none]')
+	print('My experience in machine learning is:')
+	print('[some]')
+	print('I reviewed the pre-requisite notes and carefully reviewed all associated material for topics I do not recall well:')
+	print('[yes]')
+	print('I am excited to learn the following in this class:')
+	print('[Of course! I am sooooooo excited!]')
+
+if __name__ == '__main__':
+	hw00()
